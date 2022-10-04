0f7baf8a35b296c84c930997b56fb175b759c104
setJoseEstrada <js.estrada.mx@gmail.com>
2022-10-04 14:12:53 -0500

    first

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadenas_Repetidas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string cadena = "rrtttv";
+
+           // var result = cadena.GroupBy(x=>x).Where(g=>g.Count()==1 ).Select(y=>y.Key).First();
+
+            for (int i = 0; i < cadena.Length; i++)
+            {
+                char valor = cadena[i];
+
+                if (cadena.IndexOf(valor) == cadena.LastIndexOf(valor))
+                {
+
+                    
+                        Console.WriteLine(valor);
+                    break;
+
+                 
+
+
+                }
+             
+
+
+
+            }
+
+       
+            Console.ReadKey();
+        }
+    }
+}
